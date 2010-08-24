57e96847aaf20342ea328c823b8b2ecec33b965d
Robby Grossman <robby@freerobby.com>
2010-08-23 20:58:17 -0400

    Set up bundler and test with HighLine.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'highline', '1.6.1'
+gem 'termios', '0.9.4'
---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'boot.rb'
+
+require 'highline/import'
+
+say "Welcome to <%= color('divvyr', RED) %>."
