c58c6056dbcf0f21ac01f2a1370f1c157e706f49
Robby Grossman <robby@freerobby.com>
2014-02-23 14:04:59 -0500

    Add rspec and write first tests for Buyer model.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source 'http://rubygems.org'
 gem 'highline', '1.6.1'
 gem 'ruby-termios', '~> 0.9', require: 'termios'
 
+group :development do
+  gem 'rspec'
+end
---
+++ spec/app/models/buyer_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'app', 'models', 'buyer'))
+
+describe Buyer do
+  before do
+    @buyer = Buyer.new('Joe Smith', 5)
+  end
+  describe '#number_of_games' do
+    it 'returns number of games' do
+      @buyer.number_of_games.should == 5
+    end
+  end
+  describe '#number_of_games=' do
+    it 'sets number of games' do
+      @buyer.number_of_games = 3
+      @buyer.number_of_games.should == 3
+    end
+  end
+  describe '#to_s' do
+    it 'includes name of buyer' do
+      @buyer.to_s.should include('Joe Smith')
+    end
+    it 'includes number of games' do
+      @buyer.to_s.should include('5')
+    end
+  end
+end
