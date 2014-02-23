a0861bca16defccbae8ed84a983eb99ff533bd82
Robby Grossman <robby@freerobby.com>
2014-02-23 16:42:10 -0500

    Use a highline menu for team selection.

--- app/menus/import_mlb_games_menu.rb
+++ app/menus/import_mlb_games_menu.rb
@@ -1,14 +1,25 @@
 require 'highline/import'
 
+require './app/lib/mlb_importer'
+
 class ImportMlbGamesMenu
   def self.invoke(season)
-    say 'Team IDs are as follows:'
-    MlbImporter::TEAM_IDS.each do |team, id|
-      say "#{team}: #{id}"
+    team_id = nil
+
+    choose do |menu|
+      menu.index = :letter
+      menu.index_suffix = ') '
+      menu.prompt = 'What team\'s schedule would you like to import?'
+      MlbImporter::TEAM_IDS.sort{|a,b|a<=>b}.each do |team, id|
+        menu.choice team do
+          team_id = id
+        end
+      end
     end
-    id = ask("Enter team ID: ")
-    year = ask("Enter season year to fetch: ")
-    MlbImporter.new.get_valid_games(id, year).each {|g| season.games << g}
+
+    default_year = Time.now.year
+    year = ask("Enter season year to fetch [default: #{default_year}]: ") || default_year
+    MlbImporter.new.get_valid_games(team_id, year).each {|g| season.games << g}
     say "Import complete. Season now has #{season.games.size} games."
   end
 end
